6d8d768e9d532a6f211ef46b8eb3e1b8ffdfe03a
razi <razi.uddin.qa@gmail.com>
2021-07-11 11:22:42 +0530

    FirstCommit

---
+++ src/main/java/com/temp/Main.java
@@ -0,0 +1,40 @@
+package com.temp;
+
+import io.appium.java_client.MobileElement;
+import io.appium.java_client.android.AndroidDriver;
+import io.appium.java_client.android.AndroidElement;
+import org.openqa.selenium.remote.DesiredCapabilities;
+import org.testng.Assert;
+
+import java.net.MalformedURLException;
+import java.net.URL;
+
+public class Main {
+
+    public static void main(String[] args) throws MalformedURLException {
+        // write your code here
+//        DesiredCapabilities dc = new DesiredCapabilities();
+//        dc.setCapability("deviceName", "emulator-5554");
+//        dc.setCapability("platformName", "android");
+//        dc.setCapability("appPackage", "com.google.calculator2");
+//        dc.setCapability("appActivity", ".Calculator");
+//
+//        AndroidDriver<AndroidElement> ad = new AndroidDriver<AndroidElement>(new URL("http:127.0.0.1:4723/wd/hub"), dc);
+//
+//        MobileElement el8 = (MobileElement) ad.findElementById("com.android.calculator2:id/digit_2");
+//        el8.click();
+//        MobileElement el9 = (MobileElement) ad.findElementById("com.android.calculator2:id/digit_5");
+//        el9.click();
+//        MobileElement el11 = (MobileElement) ad.findElementByAccessibilityId("plus");
+//        el11.click();
+//        MobileElement el12 = (MobileElement) ad.findElementById("com.android.calculator2:id/digit_3");
+//        el12.click();
+//        MobileElement el13 = (MobileElement) ad.findElementById("com.android.calculator2:id/digit_9");
+//        el13.click();
+//        MobileElement el14 = (MobileElement) ad.findElementByAccessibilityId("equals");
+//        el14.click();
+//
+//        Assert.assertEquals("68", ad.findElementById("com.android.calculator2:id/result").getText());
+
+    }
+}
